cb13fade57b855bc3bf3aeb9f12c4927a6a6a5f1
Ancientsl8 <61736186+Ancientsl8@users.noreply.github.com>
2022-12-05 17:51:30 +0800

    Added enemyScript
    Scripts

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] GameObject target1;
+    [SerializeField] GameObject target2;
+    [SerializeField] GameObject target3;
+    [SerializeField] GameObject target4;
+    int targetRand;
+    float moveX;
+    float moveY;
+
+    public void ChooseTarget()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
